b30f48c297ecab2020da2ff530e335fa30d8719c
Sagar Gokhale <sixtimesseven.42@gmail.com>
2013-01-25 22:21:10 -0800

    change file names to adjust for sha256 checksums.

--- lib/extensions/aws-sdk/glacier/client.rb
+++ lib/extensions/aws-sdk/glacier/client.rb
@@ -20,25 +20,43 @@ class AWS::Glacier::Client
     # for now, we'll only support simple uploads
     file_hashes = compute_hashes(path)
     
-    # {:linear_hash => linear_hash, :tree_hash => parts.first, :hashes_for_transport => parts_for_transport}
     if file_hashes[:linear_hash] != sha256sum
       raise "checksum mismatch on #{path}"
     end
     
     if parts_for_transport.length > 1
-      # this will do the multipart upload
-      raise "File #{path} too big (for now)"
+      raise "Cannot upload #{path}. Multipart uploads not yet supported."
+      # response = initiate_multipart_upload(:account_id => '-', :vault_name => vault_name, :part_size => MAX_UPLOAD_SIZE)
+      # multipart_upload_id = response.multipart_upload_id
+      
+      # raise "No multipart upload id returned! #{response.inspect}"
+      
+      # file_parts_for_upload(path) do |data, index|
+        
+      # end
+      
+      # archive_id = upload_multipart_archive(:account_id => '-', :vault_name => vault_name, :checksum => file_hashes[:tree_hash], :path => path)
+      # return archive_id
     else
       response = upload_archive(:account_id => '-', :vault_name => vault_name, :checksum => file_hashes[:tree_hash], :body => File.open(path, 'rb'))
-      raise "No archive-id returned!" unless response.archive_id
+      raise "No archive-id returned!  #{response.inspect}" unless response.archive_id
       raise "checksum sent back does not match!" unless response.checksum == file_hashes[:tree_hash]
-      
       return response.archive_id
     end
-    
   end
   
   private
+
+  def file_parts_for_upload(path, part_size=MAX_PART_SIZE)
+    file = File.open(path, 'rb')
+    index = 0
+    while(data = file.read(part_size))
+      yield(data, index)
+      index += 1
+    end
+  end
+
+
   # computes a linear and tree hash for the given file
   # will only load one meg of the file at a time.
   def compute_hashes(path, part_size = MAX_PART_SIZE)
--- lib/tasks/documents.rake
+++ lib/tasks/documents.rake
@@ -25,4 +25,32 @@ namespace :documents do
     end
   end
 
+  task :migrate_filenames => [:environment] do 
+    puts "Starting moving files."
+    total = 0
+    errors = 0
+    
+    Document.find_in_batches do |documents|
+      documents.each do |document|
+        total += 1
+        begin
+          source_filename = document.library_filename
+          extname = File.extname(source_filename)
+          dest_filename = File.join(File.dirname(source_filename), "#{document.checksum}#{extname}")
+          dirname = File.dirname(document.filename)
+          new_filename = "#{dirname}/#{document.checksum}#{extname}"
+          # puts "checksum = #{document.checksum}"
+          # puts "new filename will be #{new_filename}"
+          # puts "mv #{source_filename} to #{dest_filename}"
+          document.update_attributes!(:filename => new_filename)
+          FileUtils.mv(source_filename, dest_filename)
+        rescue Exception => e
+          errors += 1
+        end
+      end
+      puts "moved #{total} filenames. (#{errors} errors)"
+    end
+    puts "Done! moved #{total} filenames. (#{errors} errors)"
+  end
+
 end
